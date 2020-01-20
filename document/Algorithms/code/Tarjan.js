856b18ac8966e4b82a1d7b3ef9d0b25e7e96a43d
Andres Valencia Oliveros <andres.valencia@farmalisto.com.co>
2020-01-19 23:22:46 -0500

    Se depura el codigo JavaScript

--- document/Algorithms/code/Tarjan.js
+++ document/Algorithms/code/Tarjan.js
@@ -1,12 +1,9 @@
-// https://medium.com/@ziyoshams/graphs-in-javascript-cc0ed170b156
-// https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/ArticulationPoint.java
-// https://en.wikipedia.org/wiki/Biconnected_component
-
+// Test: Ubuntu 19.04 / node v10.15.2
+// Command: node /{path}/Tarjan.js
 class Graph {
-  constructor(vertices) {
-    this.V = vertices;
+  constructor() {
     this.graph = new Object();
-    this.Time = 0;
+    this.time = 0;
   }
 
   addEdge(u, v) {
@@ -22,20 +19,20 @@ class Graph {
     this.graph[v].push(u);
   }
 
-  APUtil(u, visited, ap, parent, low, disc) {
+  DFS(u = 0, visited = [], ap = [], parent = [], low = [], disc = []) {
     let children = 0;
     visited[u] = true;
 
-    disc[u] = this.Time; 
-    low[u] = this.Time;
-    this.Time += 1;
+    disc[u] = this.time; 
+    low[u] = this.time;
+    this.time += 1;
 
     for (const v of this.graph[u]) {
       if (visited[v] == false) {
         parent[v] = u;
         children += 1;
 
-        this.APUtil(v, visited, ap, parent, low, disc);
+        this.DFS(v, visited, ap, parent, low, disc);
         
         low[u] = Math.min(low[u], low[v]);
         if (parent[u] == -1 && children > 1) {
@@ -52,27 +49,28 @@ class Graph {
     }
   }
 
-  AP() {
-    var visited = new Array(this.V),
-        disc =  new Array(this.V), 
-        low =  new Array(this.V),
-        parent =  new Array(this.V),
-        ap = new Array(this.V);
+  ArticulationPoint() {
+    let V =  Object.keys(g1.graph).length; // Vertices
+    let visited = new Array(V),
+        disc =  new Array(V), 
+        low =  new Array(V),
+        parent =  new Array(V),
+        ap = new Array(V);
     
-    for (let i = 0; i < this.V; i++) 
+    for (let i = 0; i < V; i++) 
     {
         parent[i] = -1; 
         visited[i] = false; 
         ap[i] = false;
     }
     
-    for (let i = 0; i < this.V; i++) {
+    for (let i = 0; i < V; i++) {
       if (visited[i] == false) {        
-        this.APUtil(i, visited, ap, parent, low, disc); 
+        this.DFS(i, visited, ap, parent, low, disc); 
       }
     }
 
-    for (let i = 0; i < this.V; i++) {
+    for (let i = 0; i < V; i++) {
       if (ap[i] == true) {
         console.log(i + " ");
       }
@@ -82,26 +80,8 @@ class Graph {
 }
 
 // g1 = {0: [1], 1: [0, 2], 2: [1, 3], 3: [2]};
-g1 = new Graph(4);
+g1 = new Graph();
 g1.addEdge(0, 1);
 g1.addEdge(1, 2);
 g1.addEdge(2, 3);
-
-/*g1 = new Graph(5) 
-g1.addEdge(1, 0);
-g1.addEdge(0, 2);
-g1.addEdge(2, 1);
-g1.addEdge(0, 3);
-g1.addEdge(3, 4);*/
-
-/*g1 = new Graph(7);
-g1.addEdge(0, 1); 
-g1.addEdge(1, 2); 
-g1.addEdge(2, 0); 
-g1.addEdge(1, 3); 
-g1.addEdge(1, 4); 
-g1.addEdge(1, 6); 
-g1.addEdge(3, 5); 
-g1.addEdge(4, 5);*/
-g1.AP();
-//console.log(g1);
+g1.ArticulationPoint();
